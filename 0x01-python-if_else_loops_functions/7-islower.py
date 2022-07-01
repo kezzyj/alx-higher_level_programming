9e463797defeb7b023f9ba2e6b1fdb2fcc030934
kezzyj <karachidavid31@gmail.com>
2022-07-01 03:35:12 -0700

    printing alphabets in lower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 6-print_comb3.py
+
+def islower(c):
+    """Check for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+
+        return True
+
+    else:
+
+        return False

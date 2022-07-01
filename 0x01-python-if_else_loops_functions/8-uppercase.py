67473d06343fdc84410045e7857cf4e6a9855ff2
kezzyj <karachidavid31@gmail.com>
2022-07-01 04:26:41 -0700

    printing upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,4 +6,4 @@ def uppercase(str):
         if(ord(index) >= 97 and ord(index) <= 122):
             index = chr(ord(index) - 32)
         print("{}".format(index), end="")
-     print("")
+    print("")

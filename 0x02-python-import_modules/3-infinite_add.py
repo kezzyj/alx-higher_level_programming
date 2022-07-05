d564512d60b8427f5974b436578a9b67b16a3821
kezzyj <karachidavid31@gmail.com>
2022-07-05 02:49:55 -0700

    adding argv args

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+     """Print the addition of all arguments."""
+     import sys
+
+     count =  len(sys.argv) - 1
+     result = 0
+     for index in range(count):
+         result += int(sys.argv[index + 1])
+     print(f"{result}")

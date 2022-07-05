8e83f5cede6a0b0dacb4dd9613d9f6197f082764
kezzyj <karachidavid31@gmail.com>
2022-07-05 03:08:46 -0700

    adding arguements in argv

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-     """Print the addition of all arguments."""
+    """Print the addition of all arguments."""
 
-     import sys
+    import sys
 
-     count = len(sys.argv) - 1
-     result = 0
-     for index in range(count):
-         result += int(sys.argv[index + 1])
-     print(f"{result}") 
+    count = len(sys.argv) - 1
+    result = 0
+    for index in range(count):
+        result += int(sys.argv[index + 1])
+     print(f"{result}")

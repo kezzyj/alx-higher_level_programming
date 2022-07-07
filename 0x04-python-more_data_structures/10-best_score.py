513ff9fdd224bf247f01b0eb83f033d7075be71b
kezzyj <karachidavid31@gmail.com>
2022-07-07 04:38:17 -0700

    printing largest number

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,4 +5,5 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
     _key = list(sorted(a_dictionary.values()))
-    return(list(a_dictionary.keys())[list(a_dictionary.values()).index(_key[-1])])
+    return(list(a_dictionary.keys())[list(
+        a_dictionary.values()).index(_key[-1])])

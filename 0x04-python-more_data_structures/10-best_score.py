870f0ba19f173dbfcc7475eec56b9b2f6610495f
kezzyj <karachidavid31@gmail.com>
2022-07-07 04:16:21 -0700

    printing largest number

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 def best_score(a_dictionary):
      """ returns a key with the biggest integer value."""
-     if not isinstance(a_dictionary, dict) and len(a_dictionary.keys()) == 0:
+     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
          return None
      _key = list(sorted(a_dictionary.values()))
-     return(ret[-1])
+     return(_key[-1])

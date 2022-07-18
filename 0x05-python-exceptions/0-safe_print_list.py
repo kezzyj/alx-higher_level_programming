199f23579ac0d6627b833d95ba54dddf8bf76f00
kezzyj <karachidavid31@gmail.com>
2022-07-18 09:17:21 -0700

    printing above index

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -22,7 +22,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             ret += 1
 
-         except IndexError:
-             break
+        except IndexError:
+            break
     print("")
     return (ret)

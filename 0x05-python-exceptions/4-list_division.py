2e1aaa606f8f687f7a61196a446c280aa61d0149
kezzyj <karachidavid31@gmail.com>
2022-07-19 02:20:56 -0700

    didivng with several excepts

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides two lists element by element.
+    Args:
+        my_list_1 (list): The first list.
+        my_list_2 (list): The second list.
+        list_length (int): The number of elements to divide.
+
+    Returns:
+        A new list of length list_length containing all result
+        divided elements
+    """
+
+    n_list = []
+
+    for i in range(0, list_length):
+        try:
+            res = my_list[i] / my_list[i]
+        except TypeError:
+            print("wrong type")
+            res = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            res = 0
+        except IndexError:
+            print("out of range")
+            res = 0
+        finally:
+            n_list.append(res)
+    return (n_list)

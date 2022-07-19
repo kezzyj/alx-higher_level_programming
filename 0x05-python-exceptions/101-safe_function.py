edbbf0988ad2e3ff9caee62acfc9c8775c0b2400
kezzyj <karachidavid31@gmail.com>
2022-07-19 09:17:19 -0700

    checing a function safetly

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -16,6 +16,6 @@ def safe_function(fct, *args):
     try:
         func_result = fct(args)
         return (func_result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file = sys.stderr)
+    except (ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)

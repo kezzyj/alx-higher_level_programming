a9ad126653c4c883cc7fba82ad4b5e8aba868f2f
kezzyj <karachidavid31@gmail.com>
2022-07-19 02:23:40 -0700

    didivng with several excepts

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,7 +16,7 @@ def list_division(my_list_1, my_list_2, list_length):
 
     for i in range(0, list_length):
         try:
-            res = my_list[i] / my_list[i]
+            res = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             res = 0

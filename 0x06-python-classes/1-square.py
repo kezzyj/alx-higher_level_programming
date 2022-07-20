63b34f246e815c81577828ecf4e7acca27a67936
kezzyj <karachidavid31@gmail.com>
2022-07-20 05:35:29 -0700

    instantiating with init

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a square with a size"""
+
+class Square:
+    """Define a square with size"""
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size: the size of the new Square
+        """
+        self.size = size
+
+s = Square()

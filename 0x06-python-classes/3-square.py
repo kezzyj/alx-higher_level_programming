86d4bd2e1685bd2727e06b7776b2ba7b7f6ae8ae
kezzyj <karachidavid31@gmail.com>
2022-07-20 09:29:49 -0700

    getting are of square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,7 +2,8 @@
 """Defininig a square"""
 
 
-class Square: class Square.
+class Square:
+
     """Defining a square"""
     def __init__(self, size=0):
         """Initialize a new square.
@@ -14,6 +15,7 @@ class Square: class Square.
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
         def area(self):
             """Compute the current area of the square."""
             return (self.__size * self.__size)

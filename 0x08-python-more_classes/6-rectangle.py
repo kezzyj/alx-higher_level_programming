525b9ec0406183c0118fed76248ee577ea402c00
kezzyj <karachidavid31@gmail.com>
2022-07-27 02:10:24 -0700

    working with class attributes

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,17 +1,22 @@
-#!/usr/bin/python3
+#!/usr/biivin/python3
 
 """Defines a Rectangle class."""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Represent a rectangl.
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+    """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-
+        type(self).number_of_instances += 1    
         self.width = width
         self.height = height
 

031e0536c648be5c69d688c65d1953a2d8951e30
kezzyj <karachidavid31@gmail.com>
2022-08-02 09:30:41 -0700

    savig json to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,4 +8,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Write an object to a text file using JSON representation."""
     with open(filename, "w") as f:
-        f.write(json.dump(my_obj))
+        json.dump(my_obj, f)

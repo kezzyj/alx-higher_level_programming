380aceac759e8352576aa153c8e96626eefe9362
kezzyj <karachidavid31@gmail.com>
2022-10-03 15:14:34 -0700

    using slice and command line

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+
+  console.log(0);
+
+} else {
+
+  const args = process.argv.map(Number)
+
+    .slice(2, process.argv.length)
+
+    .sort((a, b) => a - b);
+
+  console.log(args[args.length - 2]);
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.

801785cb11408a241d451e097132a9c956170c90
kezzyj <karachidavid31@gmail.com>
2022-10-12 08:58:15 -0700

    fixing up

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -20,6 +20,8 @@ if __name__ == "__main__":
 
     c = db.cursor()
 
-    c.execute("SELECT * FROM `states`")
+    c.execute("SELECT * FROM `states` ORDER BY id ASC")
 
     [print(state) for state in c.fetchall()]
+    cursor.close()
+    db.close()

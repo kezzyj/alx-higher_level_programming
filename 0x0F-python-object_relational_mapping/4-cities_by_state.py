911175136ca551caf6bc455a7d37efd595286dc9
kezzyj <karachidavid31@gmail.com>
2022-10-15 06:35:06 -0700

    removing spaces between sql command

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -23,7 +23,6 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
-
     JOIN states ON cities.state_id = states.id ORDER BY cities.id")
 
     rows = cursor.fetchall()

4839497f63769303a9319114a19d246084f4fcb1
kezzyj <karachidavid31@gmail.com>
2022-10-15 05:29:37 -0700

    fixing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 
 """
@@ -19,7 +18,7 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
 
-                         passwd=argv[2], db=argv[3], charset="utf8")
+                         passwd=argv[2], db=argv[3])
 
     cursor = db.cursor()
 

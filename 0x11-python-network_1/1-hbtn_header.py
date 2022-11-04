dbbca56b723e4c74f08b3baa1a461ed540f6e021
kezzyj <karachidavid31@gmail.com>
2022-11-04 09:28:15 -0700

    getting x header id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 
 """Displays the X-Request-Id header variable of a request to a given URL.
-
 Usage: ./1-hbtn_header.py <URL>
-
 """
 
 import sys
-
 import urllib.request
-
-
-
 
 
 if __name__ == "__main__":

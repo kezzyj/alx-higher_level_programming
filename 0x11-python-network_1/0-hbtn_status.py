321edf20f2b45920933fec8478738c9c9daafb5a
kezzyj <karachidavid31@gmail.com>
2022-11-04 08:27:50 -0700

    getting response using urlib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,6 @@
 import urllib.request
 
 
-
-
-
 if __name__ == "__main__":
 
     request = urllib.request.Request("https://intranet.hbtn.io/status")
@@ -21,3 +18,5 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(body)))
 
         print("\t- content: {}".format(body))
+
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
